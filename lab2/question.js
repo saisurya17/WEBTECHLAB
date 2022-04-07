0bdb50124918084fe37deffedb9bccf993f8f8da
saisurya17 <102570675+saisurya17@users.noreply.github.com>
2022-04-07 21:05:52 +0530

    Add files via upload

---
+++ lab2/question.js
@@ -0,0 +1,48 @@
+// function oddoreve( ){
+// const num =document.getElementById("num").value;
+// console.log(num)
+//     if(num%2==0)
+//     {
+//        document.write("the num is even");
+//     }
+//     else if (num%2==1)
+//     {
+//        document.write("the num is odd")
+//     }
+
+// }
+ 
+
+ 
+// function smoddoreven( )
+// {
+//     const num= parseInt(document.getElementById("num").value);
+//     const num1=parseInt(document.getElementById("num2").value);
+//     const sum= num+num1;
+//     console.log(sum);
+//     if(sum%2==0)     {
+//         document.write("the num is even");
+//      }
+//      else if (sum%2==1)
+//      {
+//         document.write("the num is odd")
+//      }
+// }
+
+
+// for(let i=0;i<101;i++)
+// {
+//     document.write(i+" ");
+// }
+
+
+// for(let i=1;i<101;i+=2)
+// {
+//     document.write(i+" ");
+// }
+
+
+// for(let i=0;i<101;i+=2)
+// {
+//     document.write(i+" ");
+// }
